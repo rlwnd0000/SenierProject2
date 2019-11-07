04b5f22025671838cc82f9bc027edea9f2b06e0f
Jang Gi Joong <57441674+rlwnd0000@users.noreply.github.com>
2019-11-07 17:01:11 +0900

    Update NoticeModifyAction.java

--- src/ops/Notice/Action/NoticeModifyAction.java
+++ src/ops/Notice/Action/NoticeModifyAction.java
@@ -7,21 +7,16 @@ import common.Action;
 import common.ActionForward;
 import ops.Notice.Beans.Notice;
 
-
+//공고수정 폼 Action
 public class NoticeModifyAction implements Action {
-	
-	 public ActionForward execute(HttpServletRequest request,HttpServletResponse response) throws Exception{
-		 	
-		 	NoticeDetailService nds = new NoticeDetailService();
-			int n_no = Integer.parseInt(request.getParameter("n_no"));
-		    Notice n = nds.getNotice(n_no);
-		    request.setAttribute("n", n);
-		    
-		   	ActionForward forward = new ActionForward("noticeModify.jsp",false);
-	   		
-	   		return forward;
-	   		
-	 }
-
+	public ActionForward execute(HttpServletRequest request,HttpServletResponse response) throws Exception{
+		NoticeDetailService nds = new NoticeDetailService();
+		int n_no = Integer.parseInt(request.getParameter("n_no"));
+		Notice n = nds.getNotice(n_no);
+		request.setAttribute("n", n);
+		ActionForward forward = new ActionForward("noticeModify.jsp",false);
+	   	
+		return forward;
+	}
 }
 
