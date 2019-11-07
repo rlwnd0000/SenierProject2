5de7e30cde1725d683a29ad5444836e684caf664
Jang Gi Joong <57441674+rlwnd0000@users.noreply.github.com>
2019-11-07 17:05:51 +0900

    Update QuestionListAction.java

--- src/ops/Question/Action/QuestionListAction.java
+++ src/ops/Question/Action/QuestionListAction.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import common.Action;
 import common.ActionForward;
 import ops.Question.Beans.Question;
-
+//Q&A 리스트 Action
 public class QuestionListAction implements Action{
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception{
 		ActionForward forward = null;
