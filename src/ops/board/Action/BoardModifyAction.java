246489d099dcc25bfc727dcb00fb23e6a8519166
Jang Gi Joong <57441674+rlwnd0000@users.noreply.github.com>
2019-11-07 17:18:15 +0900

    Update BoardModifyAction.java

--- src/ops/board/Action/BoardModifyAction.java
+++ src/ops/board/Action/BoardModifyAction.java
@@ -6,16 +6,16 @@ import javax.servlet.http.HttpServletResponse;
 import common.Action;
 import common.ActionForward;
 import ops.board.Beans.Board;
-
+//1:1게시판 수정폼 Action
 public class BoardModifyAction implements Action {
 	public ActionForward execute(HttpServletRequest request,HttpServletResponse response) throws Exception{
 		
 		BoardDetailService bds = new BoardDetailService();
 		int board_no = Integer.parseInt(request.getParameter("board_no"));
 		Board b = bds.getBoard(board_no);
-	    request.setAttribute("b", b);
+	        request.setAttribute("b", b);
 	   	
-	    ActionForward forward = new ActionForward("boardModify.jsp",false);
+	        ActionForward forward = new ActionForward("boardModify.jsp",false);
    		return forward; 		
 	 }
 
