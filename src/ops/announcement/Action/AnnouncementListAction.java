37144eff85af888a72deb48e33538c7ff41d840c
Jang Gi Joong <57441674+rlwnd0000@users.noreply.github.com>
2019-11-07 17:11:13 +0900

    Update AnnouncementListAction.java

--- src/ops/announcement/Action/AnnouncementListAction.java
+++ src/ops/announcement/Action/AnnouncementListAction.java
@@ -9,7 +9,7 @@ import common.Action;
 import common.ActionForward;
 import ops.announcement.Beans.Announcement;
 
-
+//공지사항 리스트 Action
 public class AnnouncementListAction implements Action{
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception{
 		ActionForward forward = null;
